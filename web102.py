2ac3c6714d2c4196fa325942d9aa45b365f9ec75
Yasser Saied <ysaied80@yahoo.com>
2020-03-14 13:43:01 +0400

    test

--- web102.py
+++ web102.py
@@ -9,14 +9,22 @@ app = Flask(__name__)
 def test(uname = "test"):
     return  "username %s" % uname
 
+def check_login (username, password):
+   if (username == "ysaied"  and password ==  "yasser123"):
+      return "Welcom %s" % username
+   else:
+      return "Wrong Username or Password !!!"
+
 @app.route('/', methods=['post', 'get'])
 def login():
    if request.method == "POST":
       uname = request.form.get('username')
       pword = request.form.get('password')
+      return check_login(uname, pword)
+#      return check_login(uname, pword)
 #      return redirect(url_for('test', uname=uname))
 #      return test(uname)
-      return request.form
+#      return request.form
    return render_template('login.html')
 
 app.run(host= "172.16.67.12")
