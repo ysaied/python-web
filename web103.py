ce0e098ae63db64006fb80631fb1c69b8abf026c
Yasser Saied <ysaied80@yahoo.com>
2020-03-14 14:25:26 +0400

    pycharm

---
+++ web103.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['post', 'get'])
+def login():
+   if request.method == "POST":
+      uname = request.form.get('username')
+      pword = request.form.get('password')
+      return check_login(uname, pword)
+#      return check_login(uname, pword)
+#      return redirect(url_for('test', uname=uname))
+#      return test(uname)
+#      return request.form
+   return render_template('access.html')
