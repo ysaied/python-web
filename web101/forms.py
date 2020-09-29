137d765a58c4a6b2f988624940e2879c7d6499ec
Yasser Saied <ysaied80@yahoo.com>
2020-09-29 16:25:53 +0400

    q

--- web101/forms.py
+++ web101/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2,max=10)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4,max=10)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
