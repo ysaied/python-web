131fc610f63ff9c097e01e3e4ac28ccc331eccf8
Yasser Saied <ysaied80@yahoo.com>
2020-09-29 13:29:32 +0400

    Update forms.py

--- web101/forms.py
+++ web101/forms.py
@@ -6,14 +6,14 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Lenght(min=2,max=10)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm+password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
  #   username = StringField('Username', validators=[DataRequired(), Lenght(min=2,max=10)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-#    confirm+password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
+#    confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
