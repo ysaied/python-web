3f2aeb5e483250fa80b349e3e0d8778179f5eadb
Yasser Saied <ysaied80@yahoo.com>
2020-09-29 13:31:40 +0400

    Update forms.py

--- web101/forms.py
+++ web101/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Lenght, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Lenght(min=2,max=10)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2,max=10)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
- #   username = StringField('Username', validators=[DataRequired(), Lenght(min=2,max=10)])
+ #   username = StringField('Username', validators=[DataRequired(), Length(min=2,max=10)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
 #    confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
